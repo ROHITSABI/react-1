1f4a49348cd931fbc950fb4b44980cf852889df7
Rohit <rsabi2708@gmail.com>
2024-04-08 10:51:28 +0530

    props

--- src/App.js
+++ src/App.js
@@ -1,40 +1,14 @@
+//PROPS
+
 import "./App.css";
+import Navbar from "./components/Navbar";
+
 function App() {
   // let name = "Rohit";
+
   return (
     <>
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="/">
-            TextUtils
-          </a>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ms-auto">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/">
-                  Home
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/">
-                  About
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+      <Navbar title="TextUtils" />
     </>
   );
 }
