7371ddda7939901d664b8baa6fb9dc7866f8f507
Rohit <rsabi2708@gmail.com>
2024-04-13 20:08:12 +0530

    Navbar - We set the mode as static

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
+import { useState } from "react";
 import "./App.css";
-import About from "./components/About";
+// import About from "./components/About";
 import Navbar from "./components/Navbar";
 // import TextForm from "./components/TextForm";
-// import { useState } from "react";
 
 function App() {
-  // let name = "Rohit";
+  let [mode] = useState("dark");
 
   return (
     <>
-      <Navbar title="TextUtils" aboutText="About Us" newNumber={3} />
+      {/* <Navbar title="TextUtils" aboutText="About Us" newNumber={3} /> */}
 
       {/* This is for "Default Prop-Types" */}
       {/* <Navbar /> */}
 
       {/* This is for "isRequired" */}
-      {/* <Navbar title="TextUtils" toggleMode={toggleMode} mode={mode} /> */}
+      <Navbar title="TextUtils" mode={mode} />
 
       {/* <TextForm heading="Registration Form" mode={mode} /> */}
 
-      <About />
+      {/* <About /> */}
     </>
   );
 }
