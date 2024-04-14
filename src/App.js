b260db1835ebb3b0fc14f888837187005f066240
Rohit <rsabi2708@gmail.com>
2024-04-14 10:13:59 +0530

    Complete Mode change functionality with Alert

--- src/App.js
+++ src/App.js
@@ -8,21 +8,21 @@ import Alert from "./components/Alert";
 function App() {
   //Dark mode & Light mode through button
   //Here we use toggleMode as a 'Props'
-  let [mode, setMode] = useState("dark");
-  let [btn, setBtn] = useState("Change to Light Mode");
+  let [mode, setMode] = useState("light");
+  let [btn, setBtn] = useState("Change to Dark Mode");
   let toggleMode = () => {
-    if (mode === "dark") {
-      mode = "light";
-      setMode(mode);
-      setBtn("Change to Dark Mode");
-      document.body.style.background = "white";
-      showAlert("Light mode has been enabled", "success");
-    } else {
+    if (mode === "light") {
       mode = "dark";
       setMode(mode);
       setBtn("Change to Light Mode");
       document.body.style.background = "grey";
       showAlert("Dark mode has been enabled", "success");
+    } else {
+      mode = "light";
+      setMode(mode);
+      setBtn("Change to Dark Mode");
+      document.body.style.background = "white";
+      showAlert("Light mode has been enabled", "success");
     }
   };
   let [alert, setAlert] = useState(null);
